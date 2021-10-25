2f39073ecf1abadc7abb283c5ff11807b30269a4
Pavel <pavelcorolenko535@gmail.com>
2021-10-26 00:27:09 +0300

    day2

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ function sleep(ms) {
 }
 $(window).on("load", function () {
   setTimeout(async function () {
-    console.log("worked")
     var preloader = $(".loader");
     if ($(preloader).hasClass("done")) {
       $(preloader).addClass(" animate__fadeOut");
@@ -12,4 +11,19 @@ $(window).on("load", function () {
       $(preloader).removeClass("done loader");
     } 
   }, 400);
+
+  var stickyOffset = $('.sticky').offset().top;
+
+  $(window).scroll(function(){
+    var sticky = $('.sticky'),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= stickyOffset) {
+      sticky.addClass('fixed');
+    }
+    if (scroll <= stickyOffset) {
+      sticky.removeClass('fixed');
+    }
+  });
+
 });
